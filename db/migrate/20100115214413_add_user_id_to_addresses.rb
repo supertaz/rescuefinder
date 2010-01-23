faec8a3ec07ec9d7dc785fe3796d5b7d50b39dd2
Elad Meidar <elad@eizesus.com>
2010-01-23 17:23:17 -0500

    Fixing index names (again, screw you MySQL) and some ricochets off the merge.

--- db/migrate/20100115214413_add_user_id_to_addresses.rb
+++ db/migrate/20100115214413_add_user_id_to_addresses.rb
@@ -4,7 +4,7 @@ class AddUserIdToAddresses < ActiveRecord::Migration
     add_column :addresses, :addressable_type, :string
     add_column :addresses, :address_type_id, :integer
     remove_column :addresses, :relative_id
-    add_index :addresses, [:addressable_id, :addressable_type, :address_type_id]
+    add_index :addresses, [:addressable_id, :addressable_type, :address_type_id], :name => 'by_adrsid_adrs_typ_adrs_typ_id'
   end
 
   def self.down
